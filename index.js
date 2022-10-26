6b0733d12c10f6083cacc919ae27bb70f0142b62
Anna Levkovich <7604442@gmail.com>
2022-10-26 08:59:48 +0300

    разработка и составление класса

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-"use strict";
+// "use strict";
 
-function HashStorage() {
-        this.storage = {};
-    }
-    this.addValue = function(key, value) {
-        this.storage[key] = value;
-    }
-    this.getValue = function(key) {
-        return this.storage[key];
-    }
-    this.deleteValue = function(key) {
-        return delete this.storage[key];
-    }
-    this.getKeys = function() {
-        return Object.keys(this.storage)
-    }
-
-// class HashStorage {
-//     constructor() {
-//         this.storage = {};
+// function HashStorage() {
+//         storage = {};
 //     }
-//     addValue(key, value) {
+//     this.addValue = function(key, value) {
 //         this.storage[key] = value;
 //     }
-//     getValue(key) {
+//     this.getValue = function(key) {
 //         return this.storage[key];
 //     }
-//     deleteValue(key) {
+//     this.deleteValue = function(key) {
 //         return delete this.storage[key];
 //     }
-//     getKeys() {
+//     this.getKeys = function() {
 //         return Object.keys(this.storage)
 //     }
-// }
+
+class HashStorage {
+    constructor(coctail, proparty, ingredients, recipe) {
+        //storage = {}
+        this.coctail = coctail;
+        this.proparty = proparty;
+        this.ingredients = ingredients;
+        this.recipe = recipe;
+    }
+    addValue(key, value) {
+        this.coctail[key] = value;
+    }
+    getValue(key) {
+        return this.coctail[key];
+    }
+    deleteValue(key) {
+        return delete this.coctail[key];
+    }
+    getKeys() {
+        return Object.keys(this.coctail)
+    }
+}
 
 
-const coctailsStorage = new HashStorage();
-coctailsStorage.addValue('maxito', 'green');
+const coctailsStorage = new HashStorage('maxito');
+coctailsStorage.addValue('alcohol', 'yes');
 
-console.log(coctailsStorage.getValue('maxito'));
+console.log(coctailsStorage);
